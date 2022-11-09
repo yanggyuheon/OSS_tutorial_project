fa8c3657b05b7e9c3e661d640b3bb55517886643
yanggyuheon <gyuheon0@gmail.com>
2022-11-09 21:48:43 +0900

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base'],
-  overrides: [
-  ],
+  extends: ["airbnb-base"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: "latest",
   },
   rules: {
+    quotes: ["off", "single"],
   },
 };
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // eslint-disable-next-line object-curly-spacing
-const {RTMClient} = require('@slack/rtm-api');
-const fs = require('fs');
-const greeting = require('./greeting');
-const square = require('./square');
-require('dotenv').config();
+const { RTMClient } = require("@slack/rtm-api");
+const fs = require("fs");
+const greeting = require("./greeting");
+const square = require("./square");
+require("dotenv").config();
 
 let token;
 try {
-  token = fs.readFileSync('./token').toString('utf-8');
+  token = fs.readFileSync("./token").toString("utf-8");
 } catch (err) {
   console.error(err);
 }
-
+var test = 1;
 const rtm = new RTMClient(token);
 
 rtm.start();
-rtm.on('message', (message) => {
+rtm.on("message", (message) => {
   const { channel } = message;
   const { text } = message;
 
@@ -24,11 +24,11 @@ rtm.on('message', (message) => {
     square(rtm, text, channel);
   } else {
     switch (text) {
-      case 'hi':
+      case "hi":
         greeting(rtm, channel);
         break;
       default:
-        rtm.sendMessage('I`m alive', channel);
+        rtm.sendMessage("I`m alive", channel);
     }
   }
 });
