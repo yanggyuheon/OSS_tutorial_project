8f5aafbe0c7278b8d50b88315122d88bb244d79e
unknown <aml12333@gmail.com>
2022-11-20 15:57:51 +0900

    add scrap.js and test exported scraping function

---
+++ testscrap.js
@@ -0,0 +1,11 @@
+const scrap = require('./scrap');
+
+let result;
+(async ()=> {
+    const res = await scrap.webScraping();
+    result = res;
+})();
+
+scrap.webScraping().then(()=> {
+    console.log(result);
+});
