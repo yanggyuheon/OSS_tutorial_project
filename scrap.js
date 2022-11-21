08fac0689cef459d174107ac4db36562b3b83309
unknown <aml12333@gmail.com>
2022-11-21 12:30:52 +0900

    selector fix

--- scrap.js
+++ scrap.js
@@ -7,10 +7,12 @@ const today = new Date();
 
 const url = "https://sobi.chonbuk.ac.kr/menu/week_menu.php";
 const selector =
-  `#contents > div.contentsArea.WeekMenu > div:nth-child(227) > ` +
+  `#contents > div.contentsArea.WeekMenu` +
+  `> .section > ` +
   `div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(${
     today.getDay() + 2
   }) > ul > li`;
+
 // 요일 테스트 하시려면 getDay의 반환값이 일요일(0) ~ 토요일(6)이기 때문에 생각해서 해주세요. (기본값 2(오늘), 식단 리스트 3~7),
 // 1~2 일때 백반과 중식 적혀있는 곳은 th여서 빈배열 반환, 값이 8 초과여도 빈배열 반환되어 토요일 값이 들어가도 문제는 없어보임.
 
@@ -32,8 +34,8 @@ async function webScraping() {
   return res.filter(Boolean);
 }
 
-// webScraping().then((res)=> {
-//     console.log(res);
+// webScraping().then((res) => {
+//   console.log(res);
 // });
 
 exports.webScraping = webScraping;
