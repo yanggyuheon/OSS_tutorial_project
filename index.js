bbba9c6b141d8451f2d93538f48a53db0e35aff4
hellemon <94850119+hellemon@users.noreply.github.com>
2022-12-04 18:30:38 +0900

    기능을 입력시 기능  수행 및 기능 안내 문구 추가

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { RTMClient } = require("@slack/rtm-api");
 const fs = require("fs");
+// 사용하기 전 npm instll line-reader 할것
 const lineReader = require("line-reader");
 const greeting = require("./greeting");
 const haksa = require("./haksa");
@@ -18,6 +19,7 @@ try {
 }
 
 // 학과 배열
+
 const classarr = [];
 try {
   let i = 0;
@@ -32,6 +34,9 @@ try {
   console.error(err);
 }
 
+// 학과 사무실을 입력받을때 사용하는 flags
+let flags = 0;
+
 const rtm = new RTMClient(token);
 
 rtm.start();
@@ -39,12 +44,13 @@ rtm.start();
 rtm.on("message", (message) => {
   const { channel } = message;
   const { text } = message;
-
+  
   // switch문 정규식 사용을 위한 테스트 변경
   // case 별로 if문같이 사용
-  if (classarr.includes((text.toLowerCase()).replace(/ /g, ""))) {
+  if (classarr.includes((text.toLowerCase()).replace(/ /g, ""))&&flags === 1) {
     const idx = classarr.indexOf((text.toLowerCase()).replace(/ /g, ""));
     classresult(rtm, (text.toLowerCase()).replace(/ /g, ""), idx, channel);
+    flags = 0
   } else {
     switch (true) {
       case !Number.isNaN(Number(text)):
@@ -57,13 +63,34 @@ rtm.on("message", (message) => {
         haksa(rtm, channel, text);
         break;
       case text === "오늘 밥 뭐야":
-        scrap.todayScrap(rtm, channel);
+        if (flags === 2){
+          scrap.todayScrap(rtm, channel);
+          flags = 0
+        }
+        else{
+          rtm.sendMessage("식단 안내를 먼저 입력해주세요", channel);
+        }
         break;
+        
       case text === "이번주 뭐 나와":
-        scrap.weeklyScrap(rtm, channel);
+        if (flags === 2){
+          scrap.weeklyScrap(rtm, channel);
+          flags = 0
+        }
+        else{
+          rtm.sendMessage("식단 안내를 먼저 입력해주세요", channel);
+        }
+        break;
+      case text === "학과 안내":
+        flags = 1
+        rtm.sendMessage("학과를 입력하세요", channel);
+        break;
+      case text === "식단 안내":
+        flags = 2
+        rtm.sendMessage("이번주가 궁금하면 : '이번주 뭐 나와' 를 입력해주세요\n오늘이 궁금하면 : '오늘 뭐 나와' 를 입력해주세요", channel);
         break;
       default:
-        rtm.sendMessage("I`m alive", channel);
+        rtm.sendMessage("학과 사무실 안내를 받으시려면 : 학과 안내\n진수원 식당 안내를 받으시려면 : 식단 안내\n인사를 하고 싶으시면 : HI\n제곱을 구하시고 싶으시면 : 숫자", channel);
     }
   }
 });
