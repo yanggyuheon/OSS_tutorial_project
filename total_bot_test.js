b8ab7e73538135d8134a8f93c0c38e191d674482
hellemon <94850119+hellemon@users.noreply.github.com>
2022-12-09 18:24:02 +0900

    학사 일정 입력후 되게끔 수행 및 통합 테스트 봇 개발 진행중

--- total_bot_test.js
+++ total_bot_test.js
@@ -5,8 +5,8 @@ const { RTMClient } = require("@slack/rtm-api");
 
 const fs = require("fs");
 
-const testchannel = "D04BL0HNXM2";
-// const testuid = "U04C77L0000"
+const testchannel = "D04F5HP28NL";
+const testuid = "U04C77L0000"
 let token;
 
 
@@ -37,7 +37,7 @@ rtm.on('message', (message)=> {
     const {text} = message;
 
     console.log("받은 메시지: ", text);
-    if(true){
+    if(message.user === testuid){
         
         switch(status){
             case 1:
@@ -48,15 +48,30 @@ rtm.on('message', (message)=> {
                     console.log("테스트 #1 실패");
                     process.exit(1);
                 }
-                rtm.sendMessage("4", testchannel);
+                rtm.sendMessage("학사 일정 테스트", testchannel);
                 console.log("테스트 #2 시작");
                 break;
             case 2:
-                if(text === "16"){
+                rtm.sendMessage("학사 일정", testchannel)
+                rtm.sendMessage("12/21", testchannel)
+                if(text === "12/21는 종강 입니다."){
                     console.log("테스트 #2 성공");
                     status+=1;
                 }else{
                     console.log("테스트 #2 실패");
+                    process.exit(1);
+                }
+                rtm.sendMessage("주간 식단 테스트", testchannel);
+                console.log("테스트 #3 시작");
+                break;
+            case 3:
+                rtm.sendMessage("식단 안내", testchannel)
+                rtm.sendMessage("이번주 뭐 나와", testchannel)
+                if(text !== "undifined"){
+                    console.log("테스트 #3 성공");
+                    status+=1;
+                }else{
+                    console.log("테스트 #3 실패");
                     process.exit(1);
                 }
                 break;
@@ -66,4 +81,4 @@ rtm.on('message', (message)=> {
     }else{
         rtm.sendMessage("테스트 채널에서 떠들지 마세요.", testchannel);
     }
-})
+});
